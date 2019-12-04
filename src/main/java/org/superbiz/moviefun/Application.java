07c1e8c52957b327285f72664803384156c21676
PAL-8 <umakanthperepa@gmail.com>
2019-12-04 14:19:39 +0530

    Adding New Files

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,29 @@
+package org.superbiz.moviefun;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.activation.DataSource;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ActionServlet actionServlet){
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+
+
+    }
